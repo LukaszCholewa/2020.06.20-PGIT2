306e50588619447f45fb3cbb65524dcb735cb015
lukaszcholewa <lukasz.cholewa1@gmail.com>
2020-06-20 12:38:28 +0200

    kom

--- src/pl/camp/it/Main.java
+++ src/pl/camp/it/Main.java
@@ -1,7 +1,11 @@
 package pl.camp.it;
 
+import java.util.function.DoubleToIntFunction;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("GIT2");
+
+        System.out.println("GIT3");
     }
 }
